22aa408ecad9eaec1904ddade6de43f3b1be9fdb
IamTADI <iamtadael@gmail.com>
2023-03-17 11:30:12 -0700

    6-print_numberz_retry

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,11 @@
 
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-printf("\n");
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i + '0');
+	putchar('\n');
+
+	return (0);
 }
